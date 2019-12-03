9e11e39b45c4d455f4e71dc857747f249ea8a20d
khc196 <khc146@gmail.com>
2019-12-03 11:58:25 +0900

    update

--- authorization/admin.py
+++ authorization/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import User
 
-# Register your models here.
-admin.site.register(User)
+class UserAdmin(admin.ModelAdmin):
+    def image_tag(self, obj):
+        return format_html('<img src="{}" />'.format(obj.image.url))
+    image_tag.short_description = 'Image'
+    list_display = ['image_tag',]
 
-fields = ( 'image_tag', )
-readonly_fields = ('image_tag',)
+# Register your models here.
+admin.site.register(User, UserAdmin)
