63db4f31c9ee7eb5509d1c96d9f87c352ccd23d8
khc196 <kh146@gmail.com>
2019-12-15 19:38:08 +0900

    update

--- core/utils/firebase_message.py
+++ core/utils/firebase_message.py
@@ -9,7 +9,7 @@ def send_push_thread(user_id, data):
         if(device.type == 'android'):
             device.send_message(data=data)
         else:
-            device.send_message(data=data, title=data['title'], body=data['content'], aps=data['aps'])
+            device.send_message(data=data, title=data['title'], body=data['content'])
 def send_notification_thread(uid, notification_type, content, data):
     ref = db.reference('user/{}/notifications'.format(uid))
     key = ref.push().key
