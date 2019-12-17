9f85728e361870b616f5161bedb032bf2e8e4506
khc196 <kh146@gmail.com>
2019-12-17 21:20:28 +0900

    update

--- core/admin.py
+++ core/admin.py
@@ -4,6 +4,7 @@ from .models import *
 admin.site.register(Meeting)
 admin.site.register(Rating)
 admin.site.register(MatchRequest)
+admin.site.register(AndroidVersion)
+admin.site.register(IOSVersion)
 admin.site.register(MeetingType)
 admin.site.register(PlaceType)
-admin.site.register(Place)
