dd58088283c1dbd311026b302b18a8fa0450b3d8
khc196 <kh146@gmail.com>
2019-12-21 22:58:18 +0900

    update

--- core/admin.py
+++ core/admin.py
@@ -1,10 +1,18 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Meeting)
+
+class MeetingAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+
+class MatchRequestAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at',)
+
+admin.site.register(Meeting, MeetingAdmin)
 admin.site.register(Rating)
 admin.site.register(MatchRequest)
 admin.site.register(AndroidVersion)
 admin.site.register(IOSVersion)
 admin.site.register(MeetingType)
 admin.site.register(PlaceType)
+
